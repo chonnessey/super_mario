b55778f8a7c758429cdbf6c2cb623e7f527cd8f4
Adrian Camacho <adriancamacho18@gmail.com>
2021-08-06 12:30:03 -0700

    WIP: working on buggy save function for product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,9 @@
 class Product < ApplicationRecord
   has_many :reviews, dependent: :destroy
-  validates :name, presence: true, uniqeness: {case_sensitive: false}
+  validates :name, presence: true, uniqueness: {case_sensitive: false}
   validates :cost, presence: true
-  validates :country_of_origin, presence: true, uniqeness: {case_sensitive: false}
+  validates :country_of_origin, presence: true, uniqueness: {case_sensitive: false}
+  scope :three_most_recent, -> { order(created_at: :desc).limit(3) }
 
   before_save(:titleize_product)
 
