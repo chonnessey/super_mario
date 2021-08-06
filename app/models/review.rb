4d4c75d8e80468a38e11512a5051cf1f985903c0
Adrian Camacho <adriancamacho18@gmail.com>
2021-08-06 12:03:01 -0700

    add validations for review class

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,12 @@
 class Review < ApplicationRecord
   belongs_to :product
+  validates :author, presence: true, uniqeness: {case_sensitive: false}
+  validates :content_body, presence: true, uniqeness: {case_sensitive: false}
+
+  before_save(:titleize_author)
+
+  private
+    def titleize_author
+      self.author= self.author.titleize
+    end
 end
