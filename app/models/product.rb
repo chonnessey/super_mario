2f2b64a6d83a2ad38efeee0439ffe694bddeec98
Adrian Camacho <adriancamacho18@gmail.com>
2021-08-06 11:56:40 -0700

    add validations to product class

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,13 @@
 class Product < ApplicationRecord
   has_many :reviews, dependent: :destroy
+  validates :name, presence: true, uniqeness: {case_sensitive: false}
+  validates :cost, presence: true
+  validates :country_of_origin, presence: true, uniqeness: {case_sensitive: false}
+
+  before_save(:titleize_product)
+
+  private
+    def titleize_product
+      self.name = self.name.titleize
+    end
 end
