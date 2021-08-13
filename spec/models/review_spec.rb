f39435109c572f36bebacd5ec647a954cddb9ffd
Adrian Camacho <adriancamacho18@gmail.com>
2021-08-13 14:18:39 -0700

    add tests to check for length validations in reviews

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -5,4 +5,6 @@ describe Review do
   it { should validate_presence_of(:author)}
   it { should validate_presence_of(:rating)}
   it { should validate_presence_of(:content_body)}
+  it { should validate_length_of(:content_body) }
+  it { should validate_length_of(:rating) }
 end
