0bdc129faa1ef7d9aa86a45ab728d4d550c38ad7
Adrian Camacho <adriancamacho18@gmail.com>
2021-08-15 09:57:01 -0700

    add scope for most reviewed

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,7 +5,13 @@ class Product < ApplicationRecord
   validates :country_of_origin, presence: true
   scope :three_most_recent, -> { order(created_at: :desc).limit(3) }
   scope :made_in_usa, -> { where(country_of_origin: 'usa') }
-  scope :most_reviewed, -> {}
+  scope :most_reviewed, -> {(
+    select("products.id, products.name, products.cost, count(reviews.id) as reviews_count")
+    .joins(:reviews)
+    .group("products.id")
+    .order("reviews_count DESC")
+    .limit(1)
+    )}
 
   before_save(:titleize_product)
 
