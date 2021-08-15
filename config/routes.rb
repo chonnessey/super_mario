1d94da25e34beea2873178fca9e971a8754982cf
Adrian Camacho <adriancamacho18@gmail.com>
2021-08-15 10:58:59 -0700

    add authorization to page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   root to: 'products#landing'
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+  get '/signin' => 'sessions#new'
+  post '/signin' => 'sessions#create'
+  get '/signout' => 'sessions#destroy'
+  
   resources :products do
     resources :reviews
   end
