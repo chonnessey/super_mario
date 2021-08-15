32ee877a37c25d56782e4251f9036342cc6cec82
Adrian Camacho <adriancamacho18@gmail.com>
2021-08-15 11:24:43 -0700

    WIP: working on sign up bug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation)
+    params.require(:user).permit(:email, :password, :password_confirmation, :admin)
   end
 end
