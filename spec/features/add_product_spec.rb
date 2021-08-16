1b49c95c712bb2392a06bbb0e95178319afc180e
Adrian Camacho <adriancamacho18@gmail.com>
2021-08-15 21:28:24 -0700

    add passing integration test for adding product

--- spec/features/add_product_spec.rb
+++ spec/features/add_product_spec.rb
@@ -7,23 +7,20 @@ describe "the product creating process" do
     User.destroy_all
     test_user = User.create!({:email => "hello@hello.com", :password => "bye", :password_confirmation => "bye", :admin => false})
     test_admin = User.create!({:email => "admin@admin.com", :password => "admin", :password_confirmation => "admin", :admin => true})
-    # test_product = Product.create({:name => "Cheese", :cost => "5", :country_of_origin => "France"})
   end
 
   it "adds a new product" do
     visit signin_path
     fill_in "Email", :with => "admin@admin.com"
     fill_in "Password", :with => "admin"
-    within('.find_me') do
-      click_on ("Sign in")
-    end
+    click_on "Sign in Please"
     visit products_path
     click_link "Create new product"
     fill_in "product[name]", :with => "Cheese"
     fill_in "Cost", :with => "5"
     fill_in "product[country_of_origin]", :with => "France"
-    click_on 'Submit'
-    expect(page).to have_content 'Product successfully added!'
+    click_on 'Create Product'
+    expect(page).to have_content 'Product successfully created!'
     expect(page).to have_content "Cheese"
   end
 end
